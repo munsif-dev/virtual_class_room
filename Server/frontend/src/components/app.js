19b1971c69387ba43e6d3efc21ea0847dd58c57b
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-06 21:47:50 +0530

    start

--- Server/frontend/src/components/app.js
+++ Server/frontend/src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
 
 export default class App extends Component {
   constructor(props) {
