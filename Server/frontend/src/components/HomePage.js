744b727517d93330e21b1dfc872d4cc84a5e2a02
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-07 12:09:19 +0530

    Routing In React

--- Server/frontend/src/components/HomePage.js
+++ Server/frontend/src/components/HomePage.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import CreateRoomPage from "./CreateRoomPage";
+import JoinRoomPage from "./JoinRoomPage";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/join">Join Room</Link> |{" "}
+          <Link to="/create">Create Room</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<p>Home Page</p>} />
+          <Route path="/join" element={<JoinRoomPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
