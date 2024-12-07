5c62f40c5aa2b03bc01da01a017d09144082d52b
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-07 11:57:24 +0530

    Home page and other components are initiated

---
+++ Server/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HomePage } from "./HomePage";
+
+const App = (props) => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+// Export the App component as default
+export default App; // <-- This is necessary
--- Server/frontend/src/index.js
+++ Server/frontend/src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App"; // Import the App component from App.js
+import App from "./components/App"; // Import the App component from App.js
 
 // Get the root container element from the DOM
 const container = document.getElementById("app");
@@ -10,4 +10,4 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 
 // Render the App component inside the container
-root.render(<App />);
+root.render(<App name="munsif" />);
