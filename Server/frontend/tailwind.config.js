7b6690e54d487cc60ff5c7284de69a49d34850ea
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-09 07:53:00 +0530

    tailwind is partially added

--- Server/frontend/tailwind.config.js
+++ Server/frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./templates/**/*.html", // Include all HTML files in your templates directory
+    "./frontend/src/**/*.{js,jsx,ts,tsx}", // Include all React components in your src folder
+  ],
   theme: {
     extend: {},
   },
